fd356f74dd023c58fe930134291ba11c78077798
Jeff Khuu <heyjeffkhuu@gmail.com>
2025-04-09 20:44:18 -0600

    "How to make a slideshow" slideshow

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,19 @@
-const markdownIt = require("markdown-it");
-
 module.exports = (config) => {
     // This enable all the dependency libraries inside the `assets` folder
     config.addPassthroughCopy({
         "node_modules/reveal.js/dist": "assets/reveal/",
         "node_modules/reveal.js/plugin": "assets/reveal/plugin",
+        "node_modules/katex": "assets/katex",
     });
 
     config.addPassthroughCopy("./src/style.css");
+    config.addPassthroughCopy("./src/**/*.jpg");
+    config.addPassthroughCopy("./src/**/*.png");
+    config.addPassthroughCopy("./src/**/*.gif");
+
+    
     config.addCollection("slideshows", function(collectionApi) {
-        return collectionApi.getFilteredByGlob("./src/slides/*.md")
+        return collectionApi.getFilteredByGlob("./src/slides/**/*.md")
     })
 
     // Disable 11ty's built-in Markdown engine
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-﻿# Heading
+﻿# Team Website
+
+Website for Team 24124 (Nighthawks Robotics) from North Trail High School in Calgary, AB competing in the FIRST Tech Challenge!
+
+Simple static website using [11ty](https://www.11ty.dev/) with [reveal.js](https://revealjs.com/). Deployed using GitHub Pages.
